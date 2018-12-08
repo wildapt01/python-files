5c96fbe402dab5b8ee99bb719a242b82e24bc488
wildapt01 <pb5191@gmail.com>
2018-12-08 12:37:03 -0500

    added Model methods

--- NetNinja/django_series/djangonautic/articles/models.py
+++ NetNinja/django_series/djangonautic/articles/models.py
@@ -13,3 +13,9 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+    def snippet(self):
+        length = len(self.body)
+        if length > 50:
+            return self.body[:50] + '...'
+        return self.body
