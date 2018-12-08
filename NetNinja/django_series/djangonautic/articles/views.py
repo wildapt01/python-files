1204e3dfd11b9b30892aaa0bf1e54199dddcdad0
wildapt01 <pb5191@gmail.com>
2018-12-08 12:17:25 -0500

    added article list to article_list view

--- NetNinja/django_series/djangonautic/articles/views.py
+++ NetNinja/django_series/djangonautic/articles/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
 
 
 def article_list(request):
-    return render(request, 'articles/article_list.html')
+    data = Article.objects.all().order_by('date')
+    return render(request, 'articles/article_list.html', {'articles': data})
