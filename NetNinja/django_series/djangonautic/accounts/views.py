fa103f49538a9da1ac46c99251bd4127f5430a64
wildapt01 <pb5191@gmail.com>
2018-12-16 13:06:54 -0500

    added logic to log in a user

--- NetNinja/django_series/djangonautic/accounts/views.py
+++ NetNinja/django_series/djangonautic/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login
 
 # Create your views here.
 
@@ -8,8 +9,9 @@ def signup_view(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            form.save()
-            # Log the user in
+            # log the user in
+            user = form.save()
+            login(request, user)
             return redirect('articles:list')
     else:
         form = UserCreationForm()
@@ -21,6 +23,8 @@ def login_view(request):
         form = AuthenticationForm(data=request.POST)
         if form.is_valid():
             # log in the user
+            user = form.get_user()
+            login(request, user)
             return redirect("articles:list")
     else:
         form = AuthenticationForm()
