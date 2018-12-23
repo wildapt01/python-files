8877950760209eb3658beb8a549778e6bf4ec5c6
wildapt01 <pb5191@gmail.com>
2018-12-22 19:36:11 -0500

    added Article creation and save to DB

--- NetNinja/django_series/djangonautic/articles/views.py
+++ NetNinja/django_series/djangonautic/articles/views.py
@@ -24,6 +24,9 @@ def article_create(request):
         form = forms.CreateArticle(request.POST, request.FILES)
         if form.is_valid:
             # save article in DB
+            instance = form.save(commit=False)
+            instance.author = request.user
+            instance.save()
             return redirect("articles:list")
     else:
         form = forms.CreateArticle()
